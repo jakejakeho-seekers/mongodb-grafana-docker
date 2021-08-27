e970fbdff2768f7ca1aecf1bb0e6ae358d2ce20c
Jake Ho <mtho@connect.ust.hk>
2021-08-27 19:59:37 +0800

    Run with $PORT

--- commands.sh
+++ commands.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sed 's/3333/'"$PORT"'/' dist/server/config/default.json && npm run server
+sed 's/3333/'"$PORT"'/' dist/server/config/default.json
+cat dist/server/config/default.json
+npm run server
